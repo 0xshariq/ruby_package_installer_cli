8a36b30977bfd0bd26464b2bb86c953849e9806a
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-06 14:41:15 +0000

    Minor changes

--- bundle-standalone/dist/email-templates/collectors/index.js
+++
@@ -1,9 +0,0 @@
-// Data collection exports
-export { collectBugReportData } from './bugReport.js';
-export { collectFeatureRequestData } from './featureRequest.js';
-export { collectTemplateRequestData } from './templateRequest.js';
-export { collectQuestionData } from './question.js';
-export { collectImprovementData } from './improvement.js';
-export { collectDocsData } from './docs.js';
-export { collectCustomMessageData } from './customMessage.js';
-export { collectContactInfo, collectQuickFeedback } from './common.js';
--- bundle-standalone/dist/utils/types.js
+++
@@ -1,5 +0,0 @@
-/**
- * Type definitions for Package Installer CLI v3.2.0
- * Comprehensive type system for enhanced CLI functionality
- */
-export {};
